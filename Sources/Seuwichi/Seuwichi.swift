aa6576fbec0cee00f5991a64df660ec545e2a9a8
Conifer <64596327+conifer-dev@users.noreply.github.com>
2022-02-21 16:09:23 +0000

    Update Seuwichi.swift
    Switch voidState and State Machine to a class.

--- Sources/Seuwichi/Seuwichi.swift
+++ Sources/Seuwichi/Seuwichi.swift
@@ -5,18 +5,17 @@ public protocol State {
     mutating func onExit()
 }
 
-internal struct voidState: State {
-    mutating func update() { }
+internal class voidState: State {
+    func update() { }
 
-    mutating func render() { }
+    func render() { }
 
-    mutating func onEnter() { }
+    func onEnter() { }
 
-    mutating func onExit() { }
+    func onExit() { }
 }
 
-
-public struct StateMachine {
+open class StateMachine {
     private var states: [String: State] = [:] // Dictionary that holds all states created by user.
     public private(set) var _currentState: State = voidState() // Void state is used when state machine is initialised to avoid all the unnecessary null checks.
     public var getCurrentState: State {_currentState} // Returns what state we're currently in.
@@ -25,44 +24,48 @@ public struct StateMachine {
     public init() { }
 
     /// Add a new state to the state machine.
-    public mutating func insertState(_ state: State, withID id: String) {
-        states[id] = state
+    public func insertState(_ state: State, withID id: String) {
+        self.states[id] = state
     }
 
     /// Remove any state from the state machine.
-    public mutating func removeState(id: String) {
-        states.removeValue(forKey: id)
+    public func removeState(id: String) {
+        self.states.removeValue(forKey: id)
     }
 
     /// Remove all states from the state machine.
-    public mutating func clearStates() {
-        states.removeAll()
+    public func clearStates() {
+        self.states.removeAll()
+    }
+
+    deinit {
+        self.clearStates()
     }
 }
 
 extension StateMachine {
     /// Change from current state to another one present in the state machines dictionary.
-    public mutating func changeState(id: String) {
-        _currentState.onExit()
+    public func changeState(id: String) {
+        self._currentState.onExit()
         guard var nextState:State = states[id] else {
             assertionFailure("No state found! Please ensure you're passing the correct State ID!")
             return
         } 
         nextState.onEnter()
-        _currentState = nextState
+        self._currentState = nextState
     }
 }
 
 extension StateMachine {
     /// States own update function.
-    public mutating func update() {
-        _currentState.update()
+    public func update() {
+        self._currentState.update()
     }
 }
 
 extension StateMachine {
     /// State render function.
-    public mutating func render() {
-        _currentState.render()
+    public func render() {
+        self._currentState.render()
     }
 }
